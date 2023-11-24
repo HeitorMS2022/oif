35168bbf0b1faa292055bb9a5e945ecff9bc2d72
HeitorMS2022 <105993204+HeitorMS2022@users.noreply.github.com>
2023-11-24 13:14:46 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball;
+var ball_options;
+var groundObj;
+var leftside;
+var rightside;
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+	engine = Engine.create();
+	world = engine.world;
+	var ball_options={
+		isStatic: false,
+		restitution: 0.3,
+		friction: 0,
+		density: 1.2
+	}
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  groundObj = new ground(width/2, 670, width, 20);
+  leftside = new ground(1100, 600, 20, 120);
+  rightside = new ground(1300, 600, 20, 120)
+  groundObj.display();
+  drawSprites();
+ 
+}
+
+
+
